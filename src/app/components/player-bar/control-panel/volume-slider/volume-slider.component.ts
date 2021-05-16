40170aeb971d7395730261ac968cb8e89c10410e
ngnhut <ngnhut07051998@outlook.com>
2021-05-16 14:17:21 +0700

    Add mute/unmute button

--- src/app/components/player-bar/control-panel/volume-slider/volume-slider.component.ts
+++ src/app/components/player-bar/control-panel/volume-slider/volume-slider.component.ts
@@ -1,4 +1,8 @@
 import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
+import {
+  faVolumeUp,
+  faVolumeMute
+} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-volume-slider',
@@ -8,6 +12,8 @@ import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
 export class VolumeSliderComponent implements AfterViewInit {
 
   @ViewChild('volumeSlider') slider: ElementRef;
+
+  public volumeStatus = faVolumeUp;
 
   constructor() { }
 
@@ -22,6 +28,12 @@ export class VolumeSliderComponent implements AfterViewInit {
     const max = target.max;
     const value = target.value;
     target.style.backgroundSize = (value - min) * 100 / (max - min) + '% 100%';
+  }
+
+  onToggleVolumeStatus(): void {
+    this.volumeStatus = this.volumeStatus == faVolumeUp
+      ? faVolumeMute
+      : faVolumeUp;
   }
 
 }
