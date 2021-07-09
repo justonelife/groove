57671b5dce871f97ac18f44cdcc8680d7f4ac334
ngnhut <ngnhut07051998@outlook.com>
2021-07-09 13:30:56 +0700

    nav-init

---
+++ src/app/components/explore/explore.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ExploreComponent } from './explore.component';
+
+
+
+@NgModule({
+  declarations: [
+    ExploreComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    ExploreComponent
+  ]
+})
+export class ExploreModule { }
