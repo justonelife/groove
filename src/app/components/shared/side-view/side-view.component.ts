23970a5d2fe5f67bfb30fa8d3db80153d2ad62e6
ngnhut <ngnhut07051998@outlook.com>
2021-07-29 09:13:49 +0700

    FE_Change size

--- src/app/components/shared/side-view/side-view.component.ts
+++ src/app/components/shared/side-view/side-view.component.ts
@@ -8,7 +8,7 @@ import { Component, Input, OnInit } from '@angular/core';
 export class SideViewComponent implements OnInit {
 
   @Input() toggleBtnIcon: string;
-  public showSideView: Boolean = false;
+  public showSideView: Boolean = true;
 
   constructor() { }
 
